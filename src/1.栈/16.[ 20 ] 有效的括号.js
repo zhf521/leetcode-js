6cee8fa008c6753838d50971a77502c12133ff52
18866805256 <3407085928@qq.com>
2023-06-06 11:47:12 +0800

    第一次提交

---
+++ src/1.栈/16.[ 20 ] 有效的括号.js
@@ -0,0 +1,49 @@
+/**
+ * * 题目名称：有效的括号
+ * * 题目地址：https://leetcode-cn.com/problems/valid-parentheses
+ 
+给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效
+
+有效字符串需满足：
+
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+每个右括号都有一个对应的相同类型的左括号。
+ 
+
+示例 1：
+
+输入：s = "()"
+输出：true
+示例 2：
+
+输入：s = "()[]{}"
+输出：true
+示例 3：
+
+输入：s = "(]"
+输出：false
+ 
+
+提示：
+
+1 <= s.length <= 104
+s 仅由括号 '()[]{}' 组成
+
+
+ */
+
+// * 思路：
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {}
+
+// 测试用例
+let test = ''
+
+console.time('执行用时')
+console.log(isValid(test))
+console.timeEnd('执行用时')
